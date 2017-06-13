c4dca32fb1afc069cb1334eab6d11b667c8d86ec
xuqiang01 <xuqiang01@58ganji.com>
2017-06-13 14:28:22 +0800

    创建数据库

---
+++ app/src/main/java/com/example/xuqiang/xqweather/db/County.java
@@ -0,0 +1,47 @@
+package com.example.xuqiang.xqweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by xuqiang on 17/6/12.
+ */
+
+public class County extends DataSupport {
+    private int id;
+    private String countyName;
+    private String weatherId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    private int cityId;
+}
